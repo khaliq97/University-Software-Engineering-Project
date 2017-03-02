9950c5042afcfb9cf0047e7248e6b70a869c2f77
khaliq97 <osamakhaliq97@gmail.com>
2017-03-02 20:53:49 +0000

    CreatePersonalDetails window added.

---
+++ src/project/Views/CreatePersonalDetailsView.java
@@ -0,0 +1,56 @@
+package project.Views;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import project.Controllers.CreatePersonalDetailsController;
+import project.Controllers.HomeController;
+
+import java.io.IOException;
+
+/**
+ * Created by osamakhaliq on 02/03/17.
+ */
+public class CreatePersonalDetailsView {
+
+    HomeView homeView;
+
+    private Parent root;
+    private Stage stage;
+    private FXMLLoader fxmlLoader;
+    private Scene scene;
+
+    CreatePersonalDetailsController createPersonalDetailsController;
+
+    public CreatePersonalDetailsView(HomeView homeView)
+    {
+        this.homeView = homeView;
+        fxmlLoader = new FXMLLoader(getClass().getResource("FXML/CreatePersonalDetails.fxml"));
+        stage = new Stage();
+        createPersonalDetailsController = new CreatePersonalDetailsController(this);
+
+        fxmlLoader.setController(createPersonalDetailsController);
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        scene = new Scene(root, 414, 519);
+        stage.setTitle("Create Personal Details");
+        stage.setResizable(false);
+        stage.setScene(scene);
+    }
+
+
+    public HomeView getHomeView()
+    {
+        return homeView;
+    }
+
+    public void loadCreatePersonalDetailsView()
+    {
+        stage.show();
+    }
+}
