e5f6773c6af5ac0425a24ef15fe75e900054321d
khaliq97 <osamakhaliq97@gmail.com>
2017-03-03 12:20:14 +0000

    ReadPersonalDetailsView added.

---
+++ src/project/Views/ReadPersonalDetailsView.java
@@ -0,0 +1,85 @@
+package project.Views;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import project.Controllers.CreatePersonalDetailsController;
+import project.Controllers.ReadPersonalDetailsController;
+
+import java.io.IOException;
+
+/**
+ * Created by osamakhaliq on 02/03/17.
+ */
+public class ReadPersonalDetailsView {
+    HomeView homeView;
+
+    private Parent root;
+    private Stage stage;
+    private FXMLLoader fxmlLoader;
+    private Scene scene;
+
+    private boolean amendMode;
+
+    ReadPersonalDetailsController readPersonalDetailsController;
+
+    public ReadPersonalDetailsView(HomeView homeView)
+    {
+        this.homeView = homeView;
+        fxmlLoader = new FXMLLoader(getClass().getResource("FXML/ReadPersonalDetails.fxml"));
+        stage = new Stage();
+        readPersonalDetailsController = new ReadPersonalDetailsController(this);
+        amendMode = false;
+
+        fxmlLoader.setController(readPersonalDetailsController);
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        scene = new Scene(root, 414, 519);
+        stage.setTitle("Read Personal Details");
+        stage.setResizable(false);
+        stage.setScene(scene);
+
+    }
+
+    public HomeView getHomeView()
+    {
+        return homeView;
+    }
+
+    public boolean isAmendMode()
+    {
+        return amendMode;
+    }
+
+    public void setAmendMode(boolean amendMode)
+    {
+        this.amendMode = amendMode;
+    }
+
+
+    public ReadPersonalDetailsController getReadPersonalDetailsController()
+    {
+        return readPersonalDetailsController;
+    }
+
+    public void loadReadPersonalDetailsView()
+    {
+        stage.show();
+    }
+
+    public void loadAmendPersonalDetailsView()
+    {
+        amendMode = true;
+        stage.show();
+    }
+
+    public void closeReadPersonalDetailsView()
+    {
+        stage.close();
+    }
+}
