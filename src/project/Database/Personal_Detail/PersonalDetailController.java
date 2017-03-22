40a1b52212551463d0b832541fd3e638a971e263
khaliq97 <osamakhaliq97@gmail.com>
2017-03-22 00:49:34 +0000

    Functionality for creating a Personal Detail object added.

--- src/project/Database/Personal_Detail/PersonalDetailController.java
+++ src/project/Database/Personal_Detail/PersonalDetailController.java
@@ -1,12 +1,55 @@
 package project.Database.Personal_Detail;
-
+import project.Database.HR_Database.HRDatabaseController;
 /**
  * Created by osamakhaliq on 07/03/17.
  */
 public class PersonalDetailController {
 
-    public PersonalDetailController()
-    {
+    HRDatabaseController hrDatabaseController;
 
+    public PersonalDetailController(HRDatabaseController hrDatabaseController)
+    {
+       this.hrDatabaseController = hrDatabaseController;
+    }
+
+    public int createPersonalDetail(String username, String surname, String name, String dob, String address, String townCity,
+                                    String county, String postCode, String telephoneNumber, String mobileNumber,
+                                    String emergencyContact, String emergencyContactNumber) {
+
+        boolean personalDetailExists = false;
+        for (PersonalDetail personalDetail: hrDatabaseController.getHrDatabase().getArrayListPersonalDetails()) {
+
+            if (!personalDetail.getUsername().equals(username)) {
+                personalDetailExists = false;
+            } else {
+                personalDetailExists = true;
+            }
+        }
+
+        if (!personalDetailExists) {
+            PersonalDetail newPersonalDetail = new PersonalDetail();
+
+            newPersonalDetail.setUsername(username);
+            newPersonalDetail.setSurname(surname);
+            newPersonalDetail.setName(name);
+            newPersonalDetail.setDOB(dob);
+            newPersonalDetail.setAddress(address);
+            newPersonalDetail.setTownCity(townCity);
+            newPersonalDetail.setCounty(county);
+            newPersonalDetail.setPostcode(postCode);
+            newPersonalDetail.setTelephoneNumber(telephoneNumber);
+            newPersonalDetail.setMobileNumber(mobileNumber);
+            newPersonalDetail.setEmergencyContactName(emergencyContact);
+            newPersonalDetail.setEmergencyContactNumber(emergencyContactNumber);
+
+
+            hrDatabaseController.createPersonalDetail(newPersonalDetail);
+            return 1;
+
+
+        } else {
+            System.out.println("eijrier");
+            return 0;
+        }
     }
 }
