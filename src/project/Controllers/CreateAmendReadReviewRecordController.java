d9463114dd410993d5848a51f762940a6efcf2cd
khaliq97 <osamakhaliq97@gmail.com>
2017-03-22 00:18:18 +0000

    Checks what mode its in, Read, Amend or Create. Functionality added.

--- src/project/Controllers/CreateAmendReadReviewRecordController.java
+++ src/project/Controllers/CreateAmendReadReviewRecordController.java
@@ -1,9 +1,13 @@
 package project.Controllers;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import project.Database.Review.Review;
 import project.Views.CreateAmendReadReviewRecordView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by aman on 18/03/17.
@@ -12,6 +16,13 @@ public class CreateAmendReadReviewRecordController {
 
     CreateAmendReadReviewRecordView createAmendViewReviewRecordView;
 
+    boolean isCreateMode;
+    boolean isAmendMode;
+    boolean isReadMode;
+
+    @FXML
+    private TextField txtFieldYear;
+
     @FXML
     private TextField txtFieldStaffNumber;
 
@@ -19,25 +30,25 @@ public class CreateAmendReadReviewRecordController {
     private TextField txtFieldName;
 
     @FXML
-    private TextField txtFieldsection;
+    private TextField txtFieldSection;
 
     @FXML
-    private TextField txtFIeldManagerDirectorName;
+    private TextField txtFieldManagerDirectorName;
 
     @FXML
-    private TextField txtSecondManagerDirectorName;
+    private TextField txtFieldSecondManagerDirectorName;
 
     @FXML
-    private TextField txtFIeldObjectives;
+    private TextArea txtFieldObjectives;
 
     @FXML
-    private TextField txtFieldAchievement;
+    private TextArea txtFieldAchievement;
 
     @FXML
-    private  TextField txtFieldPreview;
+    private  TextArea txtFieldPreview;
 
     @FXML
-    private TextField txtFIeldReviewComments;
+    private TextArea txtFieldReviewComments;
 
     @FXML
     private ComboBox comboBoxRecommendation;
@@ -52,8 +63,145 @@ public class CreateAmendReadReviewRecordController {
     }
 
 
+    public void checkMode()
+    {
+        if(isCreateMode)
+        {
+            completeRecordButton.setText("Complete Record");
+
+            txtFieldStaffNumber.setEditable(true);
+            txtFieldName.setEditable(true);
+            txtFieldSection.setEditable(true);
+            txtFieldManagerDirectorName.setEditable(true);
+            txtFieldSecondManagerDirectorName.setEditable(true);
+            txtFieldObjectives.setEditable(true);
+            txtFieldAchievement.setEditable(true);
+            txtFieldPreview.setEditable(true);
+            txtFieldReviewComments.setEditable(true);
+            comboBoxRecommendation.setEditable(true);
+        }
+
+        if(isReadMode)
+        {
+            completeRecordButton.setText("Read Record");
+
+            txtFieldStaffNumber.setEditable(true);
+            txtFieldName.setEditable(false);
+            txtFieldSection.setEditable(false);
+            txtFieldManagerDirectorName.setEditable(false);
+            txtFieldSecondManagerDirectorName.setEditable(false);
+            txtFieldObjectives.setEditable(false);
+            txtFieldAchievement.setEditable(false);
+            txtFieldPreview.setEditable(false);
+            txtFieldReviewComments.setEditable(false);
+            comboBoxRecommendation.setEditable(false);
+
+        }
+    }
+    @FXML
+    public void onCompleteRecordButtonClick()
+    {
+        if(isCreateMode)
+        {
+            int result = createAmendViewReviewRecordView.getReviewController().createReviewRecord(txtFieldYear.getText(), txtFieldStaffNumber.getText(), txtFieldName.getText(), txtFieldSection.getText(), txtFieldManagerDirectorName.getText(),
+                    txtFieldSecondManagerDirectorName.getText(), txtFieldObjectives.getText(), txtFieldAchievement.getText(), txtFieldPreview.getText(),
+                    txtFieldReviewComments.getText(), comboBoxRecommendation.getSelectionModel().getSelectedItem().toString());
+
+            if(result == 1)
+            {
+                showSuccessfulReviewRecordCreationAlert(txtFieldStaffNumber.getText(), txtFieldYear.getText());
+            }else if(result == 0)
+            {
+                showReviewRecordAlreadyExists(txtFieldStaffNumber.getText(), txtFieldYear.getText());
+            }
+        }
+
+        if(isReadMode)
+        {
+            Review reviewRecordToRead = createAmendViewReviewRecordView.getReviewController().getReviewRecord(txtFieldStaffNumber.getText(), txtFieldYear.getText());
+            if(reviewRecordToRead != null)
+            {
+                txtFieldStaffNumber.setText(reviewRecordToRead.getStaffNumber());
+                txtFieldName.setText(reviewRecordToRead.getName());
+                txtFieldSection.setText(reviewRecordToRead.getSection());
+                txtFieldManagerDirectorName.setText(reviewRecordToRead.getManagerDirectorName());
+                txtFieldSecondManagerDirectorName.setText(reviewRecordToRead.getSecondManagerDirectorName());
+                txtFieldObjectives.setText(reviewRecordToRead.getObjectives());
+                txtFieldAchievement.setText(reviewRecordToRead.getAchievement());
+                txtFieldPreview.setText(reviewRecordToRead.getPreview());
+                txtFieldReviewComments.setText(reviewRecordToRead.getReviewComments());
+                if(reviewRecordToRead.getRecommendation().equals("Promotion"))
+                {
+                    comboBoxRecommendation.getSelectionModel().select(0);
+                }else if(reviewRecordToRead.getRecommendation().equals("No Promotion"))
+                {
+                    comboBoxRecommendation.getSelectionModel().select(1);
+                }
+            }else
+            {
+                showRecordDoesNotExistAlert(txtFieldStaffNumber.getText(), txtFieldYear.getText());
+            }
+
+        }
+
+        if(isAmendMode)
+        {
+
+        }
+    }
+
+    public void createMode()
+    {
+        isCreateMode = true;
+        isReadMode = false;
+        isAmendMode = false;
+
+        checkMode();
 
     }
+
+    public void readMode()
+    {
+        isReadMode = true;
+        isCreateMode = false;
+        isAmendMode = false;
+
+        checkMode();
+
+    }
+
+    public void populateComboBoxRecommendation()
+    {
+        List<String> recommendationItems = new ArrayList<String>();
+        recommendationItems.add("Promotion");
+        recommendationItems.add("No Promotion");
+
+        ObservableList observableList = FXCollections.observableList(recommendationItems);
+        comboBoxRecommendation.setItems(observableList);
+    }
+
+    public void showSuccessfulReviewRecordCreationAlert(String username, String year)
+    {
+        Alert successfulReviewRecordCreationAlert = new Alert(Alert.AlertType.INFORMATION);
+        successfulReviewRecordCreationAlert.setHeaderText("Review record successfully created for " + username + " at year " + year);
+        successfulReviewRecordCreationAlert.show();
+    }
+
+    public void showReviewRecordAlreadyExists(String username, String year)
+    {
+        Alert reviewRecordAlreadyExists = new Alert(Alert.AlertType.INFORMATION);
+        reviewRecordAlreadyExists.setHeaderText("Review record already exists for " + username + " at year " + year);
+        reviewRecordAlreadyExists.show();
+    }
+
+    public void showRecordDoesNotExistAlert(String username, String year)
+    {
+        Alert recordDoesNotExistAlert = new Alert(Alert.AlertType.ERROR);
+        recordDoesNotExistAlert.setHeaderText("Review record does not exist for " + username + " at year " + year);
+        recordDoesNotExistAlert.show();
+    }
+
+}
 
 
 
