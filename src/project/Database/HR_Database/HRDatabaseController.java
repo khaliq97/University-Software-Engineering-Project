6570d4cbd1b0ef1ae43e355efdf667fd2000ace5
khaliq97 <osamakhaliq97@gmail.com>
2017-03-25 02:12:31 +0000

    Methods added for clearing reviews and personal details ArrayList's. Comments updated.

--- src/project/Database/HR_Database/HRDatabaseController.java
+++ src/project/Database/HR_Database/HRDatabaseController.java
@@ -29,7 +29,7 @@ public class HRDatabaseController{
 
     /**
      * Returns hrDatabase class
-     * @return
+     * @return HRDatabase class
      */
     public HRDatabase getHrDatabase() {
         return hrDatabase;
@@ -37,7 +37,7 @@ public class HRDatabaseController{
 
     /**
      * Checks if HR_Database.txt File exists
-     * @return
+     * @return true if file exists, false if not
      */
     public boolean checkIfHR_DatabaseFileExists()
     {
@@ -108,6 +108,22 @@ public class HRDatabaseController{
     }
 
     /**
+     * signOffReview
+     */
+
+    public void resetPersonalDetailsArrayListForTest()
+    {
+        hrDatabase.getArrayListPersonalDetails().clear();
+    }
+
+    public void resetReviewsArrayListForTest()
+    {
+        hrDatabase.getArrayListReviews().clear();
+    }
+
+
+
+    /**
      * Creates a new PersonalDetail object
      * Adds it to hrDatabase
      * Calls write method
@@ -115,34 +131,122 @@ public class HRDatabaseController{
      */
     public void createPersonalDetail(PersonalDetail personalDetail)
     {
-        PersonalDetail personalDetailToAdd = new PersonalDetail();
         hrDatabase.getArrayListPersonalDetails().add(personalDetail);
 
         writeToDatabase();
     }
 
-
-    public void createReviewRecord(Review reviewRecord)
+    /**
+     * Sets a position in the personalDetails ArrayList in HRDatabaseController with the passed in personalDetail object.
+     * Checks if it matches its username.
+     * Writes it to the database.
+     * @param personalDetail personalDetail object that will replace the existing
+     * @return status of creating a personalDetail and writing it to the database, 1 for success, 0 for failure
+     */
+    public int amendPersonalDetail(PersonalDetail personalDetail)
     {
-
-                hrDatabase.getArrayListReviews().add(reviewRecord);
+        int returnFlag = 0;
+        int index = 0;
+        for(PersonalDetail personalDetailToCheck: hrDatabase.getArrayListPersonalDetails()) {
+            if (personalDetailToCheck.getUsername().equals(personalDetail.getUsername()))
+            {
+                hrDatabase.getArrayListPersonalDetails().set(index, personalDetail);
                 writeToDatabase();
 
+                returnFlag =  1;
+            }else
+            {
+                returnFlag =  0;
+            }
+            index++;
+        }
+
+        return returnFlag;
     }
 
-    public Review getReviewRecord(String username)
+    /**
+     * Gets PersonalDetail object from hrDatabase using userName as key
+     * @param username the user which the PersonalDetail object is gotten from
+     * @return PersonalDetail object
+     */
+    public PersonalDetail getPersonalDetail(String username)
     {
-        Review reviewToReturn = null;
-        for(Review review: hrDatabase.getArrayListReviews())
+        PersonalDetail personalDetailReturn = null;
+        for(PersonalDetail personalDetail: getHrDatabase().getArrayListPersonalDetails())
         {
 
-            if(review.getStaffNumber().equals(username))
+            if(personalDetail.getUsername().equals(username))
             {
-                reviewToReturn = review;
+                personalDetailReturn = personalDetail;
             }
         }
 
+        return personalDetailReturn;
+    }
+
+
+
+    /**
+     * Adds the passed in reviewRecord to the reviews ArrayList.
+     * Writes to the the database.
+     * @param reviewRecord reviewRecord object to add
+     */
+    public void createReviewRecord(Review reviewRecord)
+    {
+
+        hrDatabase.getArrayListReviews().add(reviewRecord);
+        writeToDatabase();
+
+    }
+
+
+    /**
+     * Gets a review object from a username and a year.
+     * @param username username of review object
+     * @param year year of review object
+     * @return Review object that matches username and year
+     */
+    public Review getReviewRecord(String username, String year)
+    {
+        Review reviewToReturn = null;
+        for(Review review: hrDatabase.getArrayListReviews()) {
+            if (review.getStaffNumber().equals(username) && review.getYear().equals(year))
+            {
+                reviewToReturn = review;
+            }else
+            {
+
+            }
+        }
         return reviewToReturn;
+    }
+
+    /**
+     * Sets a position in the reviews ArrayList in HRDatabaseController with the passed in review object.
+     * Checks if it matches its staffNumber and year.
+     * Writes it to the database.
+     * @param review review object that will replace the existing
+     * @return status of creating a review record and writing it to the database, 1 for success, 0 for failure
+     */
+    public int setReviewRecord(Review review) {
+        int returnFlag = 0;
+        int index = 0;
+        for (Review reviewToCheck : hrDatabase.getArrayListReviews()) {
+            if (reviewToCheck.getStaffNumber().equals(review.getStaffNumber()) && reviewToCheck.getYear().equals(review.getYear())) {
+                if (!review.isSigned()) {
+                    hrDatabase.getArrayListReviews().set(index, review);
+                    writeToDatabase();
+
+                    returnFlag = 1;
+                }
+
+            } else {
+                returnFlag = 0;
+            }
+            index++;
+        }
+
+        return returnFlag;
     }
 
 
